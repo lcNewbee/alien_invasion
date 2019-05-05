28d2a168a112b6c3abd3714958b9c2abbe8da926
lichao <frank.li@lixinchuxing.com>
2019-05-05 16:53:35 +0800

    feature:飞船左右移动

--- ship.py
+++ ship.py
@@ -1,9 +1,10 @@
 import pygame
 
 class Ship():
-  def __init__(self, screen):
+  def __init__(self, ai_settings, screen):
     
     self.screen = screen
+    self.ai_settings = ai_settings
 
     self.image = pygame.image.load('images/ship.bmp')
     self.rect = self.image.get_rect()
@@ -11,6 +12,19 @@ class Ship():
 
     self.rect.centerx = self.screen_rect.centerx
     self.rect.bottom = self.screen_rect.bottom
+
+    self.moving_right = False
+    self.moving_left = False
+
+    self.center = float(self.rect.centerx)
+
+  def update(self):
+    if self.moving_right:
+      self.center += self.ai_settings.ship_speed_factor
+    if self.moving_left:
+      self.center -= self.ai_settings.ship_speed_factor
+
+    self.rect.centerx = self.center
 
   def blitme(self):
     self.screen.blit(self.image, self.rect)
